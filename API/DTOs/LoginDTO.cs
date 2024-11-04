61e02af40f4f5c1904408fb7009d107781188b00
Stavros Kekakos <stnkekakos@gmail.com>
2024-11-04 15:52:21 +0200

    End of section 14

--- API/DTOs/LoginDTO.cs
+++ API/DTOs/LoginDTO.cs
@@ -5,6 +5,6 @@ namespace API.DTOs
         public string Email { get; set; }
         public string Password { get; set; }
         public string DisplayName { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
     }
 }
